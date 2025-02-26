bace6be97a8bce5bdfcb646c477cea9cafc4054d
Hamdi Rizal <hamdirizal@gmail.com>
2025-02-26 14:35:56 +0700

    Included dist folder

---
+++ dist/privileges.d.ts
@@ -0,0 +1,12 @@
+export declare const PRIVILEGE_MODULES: {
+    USERS: string;
+    ADMINS: string;
+    GENERATIONS: string;
+    PLAYLISTS: string;
+    COMMENTS: string;
+    PROMPT_PRESETS: string;
+    LYRIC_PRESETS: string;
+    PROMPT_HISTORY: string;
+    SETTINGS: string;
+    INTERNAL_API_STATS: string;
+};
---
+++ dist/privileges.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRIVILEGE_MODULES = void 0;
+exports.PRIVILEGE_MODULES = {
+    USERS: 'users',
+    ADMINS: 'admins',
+    GENERATIONS: 'generations',
+    PLAYLISTS: 'playlists',
+    COMMENTS: 'comments',
+    PROMPT_PRESETS: 'prompt_presets',
+    LYRIC_PRESETS: 'lyric_presets',
+    PROMPT_HISTORY: 'prompt_history',
+    SETTINGS: 'settings',
+    INTERNAL_API_STATS: 'internal_api_stats',
+};
