a40e5abd69c6e75f387a24539236960830d79197
Hamdi <hamdi@lalals.com>
2025-02-26 13:59:40 +0700

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,12 @@
+export const PRIVILEGE_MODULES = {
+  USERS: 'users',
+  ADMINS: 'admins',
+  GENERATIONS: 'generations',
+  PLAYLISTS: 'playlists',
+  COMMENTS: 'comments',
+  PROMPT_PRESETS: 'prompt_presets',
+  LYRIC_PRESETS: 'lyric_presets',
+  PROMPT_HISTORY: 'prompt_history',
+  SETTINGS: 'settings',
+  INTERNAL_API_STATS: 'internal_api_stats',
+};
